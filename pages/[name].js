e145dd169934f5acdba57ec412a87b18e497bf13
PrakharGupta36 <prakhargupta3680@gmail.com>
2022-03-08 22:58:03 +0530

    what is life?

--- pages/[name].js
+++ pages/[name].js
@@ -43,7 +43,6 @@ export default function Details({ list }) {
           currencies,
           languages,
         } = i;
-        console.log(i);
         return (
           <div key={index} className='card-details'>
             <div className='card-details-1'>
